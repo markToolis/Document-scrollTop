d4c3c9511a42a4e73c03e455e3314cf2c205a243
Mark Kirienko <toolismark@gmail.com>
2021-08-29 18:03:59 +0300

    add all files

---
+++ script.js
@@ -0,0 +1,6 @@
+let box = document.querySelector('.box'),
+    btn = document.querySelector('button');
+
+btn.addEventListener('click', function(){
+    document.documentElement.scrollTop = 0 ;
+});
